240b0827bc9069f60b52ca14ed3f468037fba337
Natalia Sikoń <natalii.mazur@gmail.com>
2022-11-02 10:14:26 +0100

    Create favorite list

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 
+
+
 const renderList = function (){
   const bookList = document.querySelector('.books-list');
   const source = document.querySelector('#template-book').innerHTML;
@@ -11,8 +13,27 @@ const renderList = function (){
 };
 
 const initActions = function(){
+  const bookList = document.querySelector('.books-list');
+  const favoriteBooks = [];
+
+
+  bookList.addEventListener('dblclick', (e) => {
+    e.preventDefault();
+
+    const dataId = e.target.parentElement.parentElement.dataset.id;
+
+    if (e.target.parentElement.parentElement.classList.contains('favorite')){
+      e.target.parentElement.parentElement.classList.remove('favorite');
+      favoriteBooks.splice(favoriteBooks.indexOf(dataId), 1);
+
+    } else {
+      e.target.parentElement.parentElement.classList.add('favorite');
+      favoriteBooks.push(dataId);
+    }
+  });
 
 
 };
 renderList();
 initActions();
+
